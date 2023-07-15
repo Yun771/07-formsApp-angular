1076593e545d8be2ca4359480df1fd3aa63541bb
yun <reve_1997@hotmail.com>
2023-07-15 11:48:20 -0600

    finalización formularios basicos v1

--- src/app/shared/components/sidemenu/sidemenu.component.ts
+++ src/app/shared/components/sidemenu/sidemenu.component.ts
@@ -1,37 +1,35 @@
 import { Component } from '@angular/core';
 
 interface MenuItem {
-  title: string,
-  route: string,
+  title: string;
+  route: string;
 }
-
 
 @Component({
   selector: 'shared-sidemenu',
   templateUrl: './sidemenu.component.html',
-  styleUrls: ['./sidemenu.component.css']
+  styleUrls: ['./sidemenu.component.css'],
 })
 export class SidemenuComponent {
-
-  public reactiveMenu: MenuItem[] =[
+  public reactiveMenu: MenuItem[] = [
     {
       title: 'Básicos',
-      route:'./reactive/basic'
+      route: './reactive/basic',
     },
     {
       title: 'Dinámicos',
-      route:'./reactive/basic'
+      route: './reactive/dynamic',
     },
     {
       title: 'Switches',
-      route:'./reactive/switches'
+      route: './reactive/switches',
     },
-
   ];
 
-  public authMenu: MenuItem[] = [{
-    title:'Registro',
-    route: './auth'
-  }];
-
+  public authMenu: MenuItem[] = [
+    {
+      title: 'Registro',
+      route: './auth',
+    },
+  ];
 }
